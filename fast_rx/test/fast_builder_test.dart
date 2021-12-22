f7129af4d967051b4165dff217d86a2ae91551d6
Rexios <rexios@rexios.dev>
2021-12-21 21:07:24 -0500

    Added fast_builder_test

--- fast_rx/test/fast_builder_test.dart
+++ fast_rx/test/fast_builder_test.dart
@@ -1,3 +1,19 @@
+import 'package:fast_rx/fast_rx.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
-  
+  testWidgets('FastBuilder', (WidgetTester tester) async {
+    final reactive = 0.rx;
+    await tester.pumpWidget(
+      FastBuilder(
+        builder: () =>
+            MaterialApp(home: Scaffold(body: Text('${reactive.value}'))),
+      ),
+    );
+    expect(find.text('0'), findsOneWidget);
+    reactive.value = 1;
+    await tester.pump();
+    expect(find.text('1'), findsOneWidget);
+  });
 }
