5b4da15b3e68cbe78333e1b64698c2c854639662
Rexios80 <rexios80@gmail.com>
2022-01-30 12:24:34 +0000

    Deploying to gh-pages from @ Rexios80/fast_ui@dd056a546906f618feed46d3945b908b233e9ae5 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "4d9f981d3548ff78277895cbcc15cf97",
-"/": "4d9f981d3548ff78277895cbcc15cf97",
+  "index.html": "6b49c5d8b89d9a1e538541b77f99a167",
+"/": "6b49c5d8b89d9a1e538541b77f99a167",
 "version.json": "3d86666be56e4ec6877ddb29521f792d",
 "manifest.json": "d7a845a66f3380aaa94796ae4cc65d11",
 "main.dart.js": "d9c4139f7a9b62765f4dcda6ff49973d",
