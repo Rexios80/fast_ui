44240f45eab437bbc9d87649e6ea88bec8f15a9e
Rexios <rexios@rexios.dev>
2022-06-09 10:42:26 -0400

    Cleanup

--- fast_ui/lib/fast_ui.dart
+++ fast_ui/lib/fast_ui.dart
@@ -5,5 +5,4 @@ export 'package:fast_extensions/fast_extensions.dart';
 export 'package:fast_nav/fast_nav.dart';
 export 'package:fast_overlays/fast_overlays.dart';
 export 'package:fast_rx/fast_rx.dart';
-// TODO
-// export 'package:fast_rx_persistence/fast_rx_persistence.dart';
+export 'package:fast_rx_persistence/fast_rx_persistence.dart';
