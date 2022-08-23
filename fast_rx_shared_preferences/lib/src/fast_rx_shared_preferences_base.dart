d9cf5457d3aa00d377e1875f0fab52ebafdce549
Rexios <rexios@rexios.dev>
2022-08-23 19:00:46 -0400

    Fixed test coverage

--- fast_rx_shared_preferences/lib/src/fast_rx_shared_preferences_base.dart
+++ fast_rx_shared_preferences/lib/src/fast_rx_shared_preferences_base.dart
@@ -1,6 +1,7 @@
 import 'package:fast_rx_persistence/fast_rx_persistence.dart';
 import 'package:fast_rx_shared_preferences/src/exceptions.dart';
 import 'package:fast_rx_shared_preferences/src/shared_preferences_store.dart';
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// Convenience class for initializing [FastRxPersistence] with a
@@ -24,6 +25,14 @@ class FastRxSharedPreferences {
   static Future<void> init() async {
     _prefs ??= await SharedPreferences.getInstance();
     FastRxPersistence.init(SharedPreferencesStore(prefs));
+  }
+
+  /// Reset [prefs] to null for testing
+  @visibleForTesting
+  static void reset() {
+    _prefs = null;
+    // ignore: invalid_use_of_visible_for_testing_member
+    FastRxPersistence.reset();
   }
 
   static void _checkInit() {
