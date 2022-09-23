461c748b97828224e5208923a8934bfefd6911d1
Rexios <rexios@rexios.dev>
2022-09-22 22:15:36 -0400

    ...

--- fast_rx/lib/fast_rx.dart
+++ fast_rx/lib/fast_rx.dart
@@ -3,6 +3,12 @@ library fast_rx;
 export 'src/exceptions.dart';
 export 'src/fast_builder.dart';
 
+// rx/composite
+export 'src/rx/composite/rx_composite_list.dart';
+export 'src/rx/composite/rx_composite_map.dart';
+export 'src/rx/composite/rx_composite_set.dart';
+export 'src/rx/composite/rx_composite.dart';
+
 // rx/object
 export 'src/rx/object/rx_list.dart';
 export 'src/rx/object/rx_map.dart';
