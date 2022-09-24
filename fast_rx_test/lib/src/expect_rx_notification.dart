fa1f23855373a17a584c06549b41ae5519fb857c
Rexios <rexios@rexios.dev>
2022-09-23 23:24:49 -0400

    fast_rx_persistence and fast_rx_test changes

--- fast_rx_test/lib/src/expect_rx_notification.dart
+++ fast_rx_test/lib/src/expect_rx_notification.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: implementation_imports, invalid_use_of_visible_for_testing_member
 
-import 'package:fast_rx/src/rx/rx.dart';
+import 'package:fast_rx/fast_rx.dart';
 import 'package:fast_rx_test/fast_rx_test.dart';
 import 'package:test/test.dart';
+import 'package:fast_rx/src/rx/rx_zone.dart';
 
 /// Expect that every test in [shouldNotify] notifies exactly once, and that
 /// every test in [shouldNotNotify] does not notify.
@@ -10,23 +11,24 @@ expectRxNotification<T extends Rx>({
   List<RxTest<T>> shouldNotify = const [],
   List<RxTest<T>> shouldNotNotify = const [],
 }) {
-  runTest(RxTest<T> test, int count) {
+  void runTest(RxTest<T> test, int count) {
+    var notifications = 0;
     final rx = test.generate();
-    expect(
-      rx.stream,
-      emitsInOrder([
-        ...List.generate(count, (_) => anything),
-        emitsDone,
-      ]),
+    runRxZoned(
+      () => test.transform(rx),
+      notifier: (int id) {
+        if (id == identityHashCode(rx) ||
+            (rx is RxCompositeMixin && rx.hasStream(id))) {
+          notifications++;
+        }
+      },
     );
-    test.transform(rx);
-    rx.close();
+    expect(notifications, count);
   }
 
   for (final test in shouldNotify) {
-    runTest(test, 1);
+    runTest(test, test.count);
   }
-
   for (final test in shouldNotNotify) {
     runTest(test, 0);
   }
---
+++ fast_ui/example/lib/examples/fast_rx_composite_example.dart
@@ -0,0 +1,68 @@
+import 'package:fast_ui/fast_ui.dart';
+import 'package:flutter/material.dart';
+
+class FastRxCompositeExample extends StatelessWidget {
+  final _int = 0.rx;
+
+  final _list = [0.rx].rx;
+  final _listComposite = [0.rx].rx.composite;
+
+  FastRxCompositeExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('fast_rx Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: LayoutBuilder(
+          builder: (context, constraints) => Row(
+            children: [
+              _buildList(constraints, 'RxList:', _list),
+              _buildList(constraints, 'RxCompositeList:', _listComposite),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildList(BoxConstraints constraints, String title, List list) {
+    return SizedBox(
+      width: constraints.maxWidth / 2,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title),
+          Expanded(
+            child: FastBuilder(
+              () => ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  return Text(
+                    list[index].toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void increment() {
+    _int.value++;
+
+    _list.first.value = _int.value;
+    _listComposite.first.value = _int.value;
+  }
+}
