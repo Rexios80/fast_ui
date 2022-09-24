942a865d8e8702cdfaf19f8bf1dea9c2b82188ec
Rexios <rexios@rexios.dev>
2022-09-23 21:44:23 -0400

    List tests work

--- fast_rx/test/rx/composite/rx_composite_list_test.dart
+++ fast_rx/test/rx/composite/rx_composite_list_test.dart
@@ -29,6 +29,40 @@ void main() {
       rx.run(() => rx.fillRange(0, rx.length, other), notify: false);
       other.value = 4;
     }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(() => rx.insert(0, other), notify: false);
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(() => rx.insertAll(0, [other]), notify: false);
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () => rx.replaceRange(0, rx.length, [other]),
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () => rx.setAll(0, [other]),
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () => rx.setRange(0, rx.length, [other]),
+        notify: false,
+      );
+      other.value = 4;
+    }),
   ];
 
   final shouldNotNotify = <RxTest<RxCompositeList<RxInt>>>[
@@ -71,6 +105,105 @@ void main() {
         () {
           rx.fillRange(0, rx.length, other);
           rx.fillRange(0, rx.length, 3.rx);
+        },
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () {
+          rx.add(other);
+          rx.remove(other);
+        },
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () {
+          rx[0] = other;
+          rx.removeAt(0);
+        },
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () {
+          rx.add(other);
+          rx.removeLast();
+        },
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () {
+          rx.add(other);
+          rx.removeRange(0, rx.length);
+        },
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () {
+          rx.add(other);
+          rx.removeWhere((_) => true);
+        },
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () {
+          rx.add(other);
+          rx.replaceRange(0, rx.length, []);
+        },
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () {
+          rx.add(other);
+          rx.retainWhere((_) => false);
+        },
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () {
+          rx.add(other);
+          rx.setAll(0, [1.rx, 2.rx]);
+        },
+        notify: false,
+      );
+      other.value = 4;
+    }),
+    RxTest(() => [1.rx].rx.composite, (rx) {
+      final other = 2.rx;
+      rx.run(
+        () {
+          rx.add(other);
+          rx.setRange(0, rx.length, [1.rx, 2.rx]);
         },
         notify: false,
       );
