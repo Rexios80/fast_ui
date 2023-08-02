438a73230f8e1e5177445aacc673532a2164c2cf
Rexios <rexios@rexios.dev>
2023-08-02 16:28:26 -0400

    Navigation context extensions

--- fast_extensions/CHANGELOG.md
+++ fast_extensions/CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.4
+- Adds `FastNavigation`
+
 ## 0.1.3
 - Adds `FastMaterialStateProperty`
 
--- fast_extensions/lib/fast_extensions.dart
+++ fast_extensions/lib/fast_extensions.dart
@@ -5,4 +5,5 @@ export 'src/fast_material_color.dart';
 export 'src/fast_material_localizations.dart';
 export 'src/fast_material_state_property.dart';
 export 'src/fast_media_query.dart';
+export 'src/fast_navigation.dart';
 export 'src/fast_theme.dart';
