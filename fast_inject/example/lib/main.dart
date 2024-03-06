0353574bf6e3da6b2861b6ff0f913fb6490d5b4a
Rexios <rexios@rexios.dev>
2024-03-06 14:37:10 -0500

    Working on example

--- fast_inject/example/lib/main.dart
+++ fast_inject/example/lib/main.dart
@@ -1,20 +1,41 @@
-import 'package:fast_inject_example/deps.dart';
-import 'package:fast_inject_example/nested.dart';
-import 'package:fast_inject_example/stateful.dart';
-import 'package:fast_inject_example/stateless.dart';
+import 'package:fast_inject/fast_inject.dart';
+import 'package:fast_nav/fast_nav.dart';
+import 'package:fast_rx_flutter/fast_rx_flutter.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        // body: Stateless()
-        // body: Stateful()
-        body: Nested()
-          ..inject(Dep1())
-          ..inject(Dep2())
-          ..inject(Dep3()),
-      ),
+      navigatorKey: FastNav.init(),
+      home: Counter()..inject(CountController()),
     ),
   );
+}
+
+class CountController {
+  final count = 0.rx;
+}
+
+class Counter extends FastWidget {
+  late final controller = get<CountController>();
+
+  Counter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FastBuilder(() => Text('${controller.count}')),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => controller.count.value++,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
